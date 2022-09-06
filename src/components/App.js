9be5e95101185ca497e006407fe8c7dd70365f3b
Suresh Dukiya <rjsh1503@gmail.com>
2022-09-06 23:52:24 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">E-Commerce Website</h1>
+      <p data-ns-test="project-description">Description : This will be a clone of E-commerce website. It will contain all the functionalities which a real website have. <br/>Using react the website can be divided into number of components. React as well javascript basics will be cleared using this website. <br/>Data will be displayed by fetching a api.</p>
+    </div>
   )
 }
 
